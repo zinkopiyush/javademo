564282a72f01728e26c7c2bd282af369dd1257b5
zinkopiyush <piyushshende484@gmail.com>
2026-04-16 16:54:20 +0530

    fixed the button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function changecolor(){
-    document.getElementById("changecolor").addEventListener('click', () => {
+   
         document.getElementById("welcomeaboard").style.color = 'red';
         document.getElementById("welcomeaboard").innerHTML = 'button works!!!';
-    })
-}
+    }
+
 
 

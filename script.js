5dc16398c384e8e1dbc827a5285c4a4390575bb5
zinkopiyush <piyushshende484@gmail.com>
2026-04-16 15:46:24 +0530

    added a text color button

---
+++ script.js
@@ -0,0 +1,6 @@
+function changecolor(){
+    document.getElementById("changecolor").addEventListener('click', () => {
+        document.getElementById("welcomeaboard").style.color = 'red';
+        document.getElementById("welcomeaboard").innerHTML = 'button works!!!';
+    })
+}

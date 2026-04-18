415ab572b6dd866fdb35815490925fc57215a274
zinkopiyush <piyushshende484@gmail.com>
2026-04-18 20:30:11 +0530

    tried something new

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ function agefunction(){
     if(isNaN(Number(document.getElementById('inputhere').value))){
         adult = "input is not a number";
     }
+    else if(
+        adult = (Number(document.getElementById("inputhere").value) > 18)){ adult = "u r mature.";
+    }
     else{
-        adult = (Number(document.getElementById("inputhere").value) > 18) ? "u r mature." : "sry, not old enough";
+        adult = "sry, not old enough"
     }
     document.getElementById("output").innerHTML = adult;
 }
+
+    if(new Date().getHours() < 18){
+        document.getElementById("greetings").innerHTML = "GOOD MORNING";
+    }
